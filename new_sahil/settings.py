e13bb0748c13b175618add0ecee338b2ff72d1f3
sanjay <sanjaypython22@gmail.com>
2024-10-26 22:30:01 +0530

    p

--- new_sahil/settings.py
+++ new_sahil/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'new_sahil.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'mbdb/db.sqlite3',
     }
 }
 
